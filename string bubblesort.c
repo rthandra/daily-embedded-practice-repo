a20c4942d6838c8b8d233d773509983339cd2b5f
Rushika Thandra <109749791+rthandra@users.noreply.github.com>
2026-01-14 14:48:37 -0800

    string bubblesort.c

--- string bubblesort.c
+++ string bubblesort.c
@@ -27,3 +27,52 @@ int main() {
   bubbleSort(s);
   return 0;
 }
+/*
+//optimize
+void bubbleSort(char *s){
+  int len = strlen(s);
+  int count = 0;
+
+  for(int j = 0; j<len;j++){
+    for(int i = 1; i < len; i++){
+      if(s[i] < s[i-1]){
+        char temp = s[i-1];
+        s[i-1] = s[i];
+        s[i] = temp;
+      }
+      else{
+        count++;
+      }
+      if(count == len-1){
+        printf("count = %d\n", count);
+        break;
+      }
+
+    }
+    count = 0;//reset count after every call
+  }
+
+  
+  for(int i = 0; i < len; i++){
+    printf("%c\n", s[i]);
+  }
+}
+
+int main() {
+
+  char s[] = "dbca";
+  bubbleSort(s);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+*/
