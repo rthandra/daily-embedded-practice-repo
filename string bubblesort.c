66ec8d5cbd6ad832a7a9b6118871bcf6149aa5e0
Rushika Thandra <109749791+rthandra@users.noreply.github.com>
2026-01-14 14:36:31 -0800

    string bubblesort.c

---
+++ string bubblesort.c
@@ -0,0 +1,29 @@
+/*
+ every pass - you comopare every element to its prev
+ you do n-1 pass, so every element in string is sorted once
+*/
+
+void bubbleSort(char *s){
+  int len = strlen(s);
+
+  for(int j = 0; j<len;j++){
+    for(int i = 1; i < len; i++){
+      if(s[i] < s[i-1]){
+        char temp = s[i-1];
+        s[i-1] = s[i];
+        s[i] = temp;
+      }
+    }
+  }
+
+  for(int i = 0; i < len; i++){
+    printf("%c\n", s[i]);
+  }
+}
+
+int main() {
+
+  char s[] = "dbca";
+  bubbleSort(s);
+  return 0;
+}
