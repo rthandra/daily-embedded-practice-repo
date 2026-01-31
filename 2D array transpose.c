892f1b34152a83ba02ced7f9a94eeb6971fc94c7
Rushika Thandra <109749791+rthandra@users.noreply.github.com>
2026-01-31 14:04:35 -0800

    2D array transpose.c

--- 2D array transpose.c
+++ 2D array transpose.c
@@ -21,3 +21,32 @@ int main() {
   transpose(n, arr);
   return 0;
 }
+//rectangular array
+/*
+void transpose(int n, int m, int arr[n][m]){
+
+  int matrix[m][n];
+
+  for(int i = 0; i < m; i++){ // you want m rows, n cols
+    for(int j = 0; j < n; j++){
+      matrix[i][j] = arr[j][i]; //transpose
+    }
+  }  
+
+   for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+      printf("%d", matrix[i][j]);
+    }
+ }
+}
+
+int main() {
+  int n = 3;
+  int m = 2;
+  int arr[3][2] = {{1,2}, {4,5}, {7,8}}; //147, 258
+  transpose(n, m, arr);
+  return 0;
+}
+
+
+*/
